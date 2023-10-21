394b300f48b0245a009506da3fe9ff0692c78436
HR0N <vivante.des@gmail.com>
2023-10-21 03:43:18 +0300

    init

---
+++ index.php
@@ -0,0 +1,233 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Meta-тег "nofollow" для заборони індексації пошуковими системами -->
+    <meta name="robots" content="nofollow">
+
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <!--    CSS     -->
+    <link rel="stylesheet" href="css/settings.css">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/section-education.css">
+    <link rel="stylesheet" href="css/section-prepare.css">
+    <link rel="stylesheet" href="css/section-experience.css">
+    <link rel="stylesheet" href="css/section-online-pgogram.css">
+    <link rel="stylesheet" href="css/section-chats.css">
+    <link rel="stylesheet" href="css/section-reviews.css">
+    <!--    h-bootstrap     -->
+    <link rel="stylesheet" href="libs/h-bootstrap/node_modules/h-bootstrap/h-boobstrap.css">
+    <!--    h-icons     -->
+    <link rel="stylesheet" href="libs/hicons/hicons-v1.0/style.css">
+</head>
+<body>
+<header>
+    <nav>
+        <ul>
+            <li class="hicon-xmark close toggle"></li>
+            <li class="hicon-bars open toggle"></li>
+            <li><a href="#experience">Досвід</a></li>
+            <li><a href="#program">Програма</a></li>
+            <li><a href="#chats">Учні</a></li>
+            <li><a href="#reviews">Відгуки</a></li>
+        </ul>
+    </nav>
+</header>
+
+<section class="section section-education">
+    <div class="col1">
+        <div class="title1">Онлайн навчання<br>сучасним технікам<br>фарбування<br>для перукарів<br>від майстра Лілії Кірдяк</div>
+        <div class="title2">MAMABLONDA</div>
+        <div class="description">
+            <span>Майстер-класи</span>
+            <span>Вебінари</span>
+            <span>Онлайн-курси</span>
+            <span>Офлайн-курси</span>
+        </div>
+        <div class="link-btn">ЗАПОВНИТИ АНКЕТУ ПЕРЕДЗАПИСУ</div>
+    </div>
+    <div class="col2"><img src="img/MAMABLONDA%201.png" alt="MAMABLONDA"></div>
+</section>
+
+<section class="section section-prepare">
+    <div class="section-prepare__wrapper">
+        <div class="title">
+            Готую <span class="gradient1">спеціалістів</span><br>
+            <span class="gradient2">в профессіі та з 0</span>, орієнтованих<br>
+            на досягнення результатів
+        </div>
+        <div class="description">В основі навчальних програм лежать лише сучасні практики,<br>
+            які підтверджені моїм 20-річним досвідом.</div>
+    </div>
+</section>
+
+<section class="section section-experience" id="experience">
+    <div class="section-wrapper">
+        <div class="col1">
+            <div class="item item1">
+                <div class="count">10000+</div>
+                <div class="description">задоволених кліентів<br> Лілії Кірдяк</div>
+            </div>
+            <div class="item item2">
+                <div class="count">10+</div>
+                <div class="description">проведених курси</div>
+            </div>
+            <div class="item item3">
+                <div class="count">1</div>
+                <div class="description">Онлайн курс, який в якому<br> зібрано весь досвід та знання.</div>
+            </div>
+            <div class="item item4">
+                <div class="count">100+</div>
+                <div class="description">выпускників школи<br> колористів MAMABLONDA</div>
+            </div>
+        </div>
+        <div class="col2">
+            <div class="row row1">
+                <div class="percent">97.1%</div>
+                <div class="description">Наших учеників повідомили, що<br> отримали від курсів більше информації,
+                    <br> ніж очікували</div>
+            </div>
+            <div class="row row2">
+                <div class="percent">78.2%</div>
+                <div class="description">Збільшили середній чек на послуги та<br> особистий дохід після проходження<br>
+                    курсів</div>
+            </div>
+            <div class="row row3">
+                <div class="percent">67.1%</div>
+                <div class="description">Наших учеників почали працювати на себе<br> після курсу, почавши
+                    використовувати<br> вивчені практики</div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="section section-online-program" id="program">
+    <div class="section-wrapper">
+        <div class="block1">
+            <div class="col1">
+                <div class="title1">Програма </div>
+                <div class="title2">онлайн курсу</div>
+                <div class="description">Беремо вектор на розвиток власних компетенцій у галузі колористики<br>
+                    для кожного рівня фахівця, навіть якщо ви тільки прийшли в професію.</div>
+            </div>
+            <div class="col2">
+                <img src="img/div.zero-layer-frame.webp" alt="hairdresser tool">
+            </div>
+        </div>
+        <div class="block2">
+            <div class="row row1">
+                <div class="title1">"АIRTOUCH"<br>&nbsp;безшовний<br>&nbsp;авторський</div>
+                <div class="line"></div>
+                <div class="description1">Айртач,пакування,нанесення,</div>
+                <div class="description2">баланс в % співвідношенні блонду!!</div>
+            </div>
+            <div class="row row2">
+                <div class="title">"АIRTOUCH+КОНТУРІНГ"</div>
+                <div class="description1">останні тенденції</div>
+                <div class="line"></div>
+                <div class="description2">Контурінг на натуральне волосся технікою АIRTOUCH</div>
+                <div class="dontKnow"></div>
+                <div class="scissors">
+                    <img src="img/scissors.webp" alt="hairdresser tool">
+                </div>
+            </div>
+            <div class="row row3">
+                <div class="title">"Back to Back" </div>
+                <div class="description1">контурінг метод</div>
+                <div class="line"></div>
+                <div class="description2">З тотал блонду в розтяжку Дімаут+Контурінг<br>«Ефект Омбре»</div>
+            </div>
+        </div>
+        <div class="block3">
+            <div class="link-btn">ЗАПОВНИТИ АНКЕТУ ПЕРЕДЗАПИСУ</div>
+        </div>
+    </div>
+</section>
+
+<section class="section section-chats" id="chats">
+    <div class="section-wrapper">
+        <div class="block-1">
+            <div class="col col-1">
+                <div class="title">20 років</div>
+                <div class="description">досвіду роботи в професії</div>
+            </div>
+            <div class="col col-2">
+                <div class="title">5 років</div>
+                <div class="description">викладання</div>
+            </div>
+            <div class="col col-3">
+                <div class="title">10+</div>
+                <div class="description">живих семінарів</div>
+            </div>
+            <div class="col col-4">
+                <div class="title">100+</div>
+                <div class="description">навчених спеціалістів<br> в Україні, Европі та США</div>
+            </div>
+        </div>
+        <div class="block-2">
+            <div class="col-1">
+                <div class="titles">
+                    <div class="title-1">Нас поєднують</div>
+                    <div class="title-2">чати учнів</div>
+                </div>
+                <div class="video"><iframe src="https://www.youtube.com/embed/K4TOrB7at0Y?si=lsY7E2OykX_BD0-0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></div>
+            </div>
+            <div class="col-2">
+                <div class="row row-1">Отримуйте доступ до чатів перукарів-колористів,
+                    приєднуючись до онлайн-курсів. Це місце, де
+                    зібралися випускники та нові учасники, а також
+                    фахівці з усього світу для підвищення
+                    власного рівня кваліфікації та взаємодопомоги</div>
+                <div class="row row-2">Будьте на зв'язку з однодумцями,
+                    обмінюйтесь знаннями, досвідом та
+                    ідеями.</div>
+                <div class="row row-3">Знаходьте партнерів, підрядників або
+                    співробітників.</div>
+                <div class="row row-4">Дізнайтесь про актуальні техніки
+                    фарбування та фішки зі світу
+                    колористики.</div>
+                <div class="row row-5">Отримуйте зворотний зв'язок від учасників
+                    групи та Лілії Кірдяк особисто.</div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="section section-reviews" id="reviews">
+    <div class="section-wrapper">
+        <div class="col-1"></div>
+        <div class="col-2">
+            <div class="title">Результати<br>та <span class="gradient">відгуки</span><br>учениць<br><b>Лілії Кірдяк</b></div>
+            <div class="description">Все ще сумніваєтеся в тому, що ця освіта є корисною?</div>
+            <div class="btn-link">Заповнити анкету передзапису</div>
+        </div>
+    </div>
+</section>
+
+<footer>
+    <div class="row row-1">
+        <div class="social-networks">
+            <a href="#"><div class="hicon-telegram"></div></a>
+            <a href="#"><div class="hicon-whatsapp"></div></a>
+            <a href="#"><div class="hicon-viber"></div></a>
+            <a href="#"><div class="hicon-instagram"></div></a>
+        </div>
+        <div class="rights">
+            <div>© 2023  MAMABLONDA / Лілія Кірдяк.</div>
+            <br>
+            <div>Всі права захищено.</div>
+        </div>
+    </div>
+    <div class="row row-2"><img src="img/MAMABLONDA%201.png" alt="MAMABLONDA"></div>
+</footer>
+</body>
+<script src="libs/jquery/jquery.js"></script>
+<script src="js/Father.js"></script>
+<script src="js/header.js"></script>
+</html>
---
+++ js/env.js
@@ -0,0 +1,8 @@
+class Env {
+    constructor() {
+        this.sBot_token = "00000000000000";
+        this.sGroup_id = "00000000000000";
+    }
+    bot_token(){return this.sBot_token;}
+    group_id(){return this.sGroup_id;}
+}
